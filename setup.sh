b2f63fae76362d33e9f4cdb1347716ed31067122
Silke pilon <silkepilon2009@gmail.com>
2023-04-24 12:27:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
 
-is_valid_sk_key() {
-  local api_key=$1
-  local pattern="^sk-[a-zA-Z0-9]{48}$"
-  [[ $api_key =~ $pattern ]] && return 0 || return 1
-}
 
-echo -n "Enter your OpenAI Key (eg: sk...) or press enter to continue with no key: "
-read OPENAI_API_KEY
 
-if is_valid_sk_key $OPENAI_API_KEY || [ -z "$OPENAI_API_KEY" ]; then
-  echo "Valid API key"
-else
-  echo "Invalid API key. Please ensure that you have billing set up on your OpenAI account"
-  exit
-fi
 
 NEXTAUTH_SECRET=$(openssl rand -base64 32)
 
 ENV="NODE_ENV=production\n\
 NEXTAUTH_SECRET=$NEXTAUTH_SECRET\n\
 NEXTAUTH_URL=http://localhost:3000\n\
-OPENAI_API_KEY=$OPENAI_API_KEY\n\
+OPENAI_API_KEY=\n\
 DATABASE_URL=file:../db/db.sqlite\n"
 
 printf $ENV > .env
@@ -30,8 +17,8 @@ printf $ENV > .env
 if [ "$1" = "--docker" ]; then
   printf $ENV > .env.docker
   source .env.docker
-  docker build --build-arg NODE_ENV=$NODE_ENV -t agentgpt .
-  docker run -d --name agentgpt -p 3000:3000 -v $(pwd)/db:/app/db agentgpt
+  docker build --build-arg NODE_ENV=$NODE_ENV -t youagent .
+  docker run -d --name youagent -p 3000:3000 -v $(pwd)/db:/app/db youagent
 else
   printf $ENV > .env
   ./prisma/useSqlite.sh
